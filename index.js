36d7e3ec4a04882036a31cc7064a37bb12287fba
rdheadyii <rdheadyii@gmail.com>
2023-07-23 08:04:01 -0400

    added another variable to import generate markdown and wrote functions needed to write and init

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// TODO: Include packages needed for this application
+// imported packages needed 
 const inquirer = require('inquirer');
 const fs = require('fs');
-// TODO: Create an array of questions for user input
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// questions array for user input
 const questions = [
     {
         type: "input",
@@ -56,11 +58,20 @@ const questions = [
     }
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('Success!'))
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then(function(data) {
+            writeToFile("readme.md", generateMarkdown(data));
+            console.log(data);
+        })
+}
 
 // Function call to initialize app
 init();
